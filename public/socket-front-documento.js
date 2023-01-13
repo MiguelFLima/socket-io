51d1761512f5543474ba17f946f85fd610564ece
Miguel <miguel2014ssa@gmail.com>
2023-01-13 10:27:04 -0300

    first comunication/ updating #textoEditor.value

---
+++ public/socket-front-documento.js
@@ -0,0 +1,9 @@
+import { atualizaTextoEditor } from './documento.js';
+
+const socket = io();
+
+export function emitirTextoEditor(texto) {
+  socket.emit('texto_editor', texto);
+}
+
+socket.on('texto_editor_clientes', (text) => atualizaTextoEditor(text));
