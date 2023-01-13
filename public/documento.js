f72c3fa0a5d932f90da36b2d8de6c489de3bc3cb
Miguel <miguel2014ssa@gmail.com>
2023-01-13 11:59:40 -0300

    rooms/saving changes/ rendering dynamically

--- public/documento.js
+++ public/documento.js
@@ -1,9 +1,24 @@
-import { emitirTextoEditor } from './socket-front-documento.js';
+import {
+  emitirTextoEditor,
+  selecionarDocumento,
+} from './socket-front-documento.js';
+
+const parametros = new URLSearchParams(window.location.search);
+const nomeDocumento = parametros.get('nome');
 
 const textoEditor = document.querySelector('#editor-texto');
+const tituloDocumento = document.querySelector('#titulo-documento');
+
+selecionarDocumento(nomeDocumento);
+
+tituloDocumento.textContent =
+  `Documento ${nomeDocumento}` || 'Documento sem título';
 
 textoEditor.addEventListener('keyup', () => {
-  emitirTextoEditor(textoEditor.value);
+  emitirTextoEditor({
+    texto: textoEditor.value,
+    nomeDocumento,
+  });
 });
 
 export function atualizaTextoEditor(texto) {
